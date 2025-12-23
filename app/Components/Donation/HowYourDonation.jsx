82b434b7a85459b0280b0011e1030bbf79125f9e
BilalvakaniBidecsol <bilal@bidecsol.com>
2025-12-23 13:44:05 -0800

    chnag

--- app/Components/Donation/HowYourDonation.jsx
+++ app/Components/Donation/HowYourDonation.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import DOMPurify from "dompurify";
+import DOMPurify from "isomorphic-dompurify";
 import { Image_Url } from "../../../Utils/const";
 
 const HowYourDonation = ({ donationPageHowYourDonationHelp, pageName }) => {
@@ -10,7 +10,7 @@ const HowYourDonation = ({ donationPageHowYourDonationHelp, pageName }) => {
 
   return (
     <section className="w-full px-6 md:px-20 py-10 flex flex-col md:flex-row items-center gap-10">
-      
+
       {/* Left Side Text */}
       <div className="md:w-2/2 text-center md:text-start">
         <h2 className="text-2xl md:text-3xl font-Amaranth text-black leading-snug md:leading-tight">
@@ -19,16 +19,16 @@ const HowYourDonation = ({ donationPageHowYourDonationHelp, pageName }) => {
 
         {pageName === "donation" ? (
           <div
-  className="font-inter text-sm text-[#444] mt-3 md:mt-4 "
-  dangerouslySetInnerHTML={{
-    __html: DOMPurify.sanitize(
-      description
-        // Add Tailwind classes directly into HTML for bullets
-        .replaceAll("<ul>", "<ul class='list-disc list-inside space-y-2'>")
-        .replaceAll("<li>", "<li class='text-sm text-[#444]'>")
-    ),
-  }}
-></div>
+            className="font-inter text-sm text-[#444] mt-3 md:mt-4 "
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(
+                description
+                  // Add Tailwind classes directly into HTML for bullets
+                  .replaceAll("<ul>", "<ul class='list-disc list-inside space-y-2'>")
+                  .replaceAll("<li>", "<li class='text-sm text-[#444]'>")
+              ),
+            }}
+          ></div>
 
         ) : (
           <div className="flex flex-col gap-3 py-4 text-[#777777] text-sm md:text-base">
